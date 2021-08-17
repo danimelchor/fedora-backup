3dcde6910950465bdecd3fe6dd4931c6565453c4
Daniel Melchor <danielmelchor@localhost.localdomain>
2021-08-17 12:12:51 +0200

    initial commit

---
+++ newfedora.sh
@@ -0,0 +1,10 @@
+sudo dnf install snapd
+# Log out and log in
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install snap-store
+
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install spotify
+sudo snap install brave
+sudo dnf install gnome-tweaks
